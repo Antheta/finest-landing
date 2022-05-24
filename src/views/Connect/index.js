200f9b7f75fb45198fe4b108de0b4dddb823a0f2
Marcos Raudkett <info@marcosraudkett.com>
2022-05-24 20:48:08 +0300

    Connect view updates

--- src/views/Connect/index.js
+++ src/views/Connect/index.js
@@ -1,22 +1,44 @@
-import { Badge, Box, Center, Container, Grid } from "@chakra-ui/react"
+import { Badge, Box, Button, Center, Container, Flex, Grid, Stack, Text } from "@chakra-ui/react"
+
+import { SiMessenger, SiTelegram, SiSlack } from 'react-icons/si';
 
 const Connect = () => {
     return (
-        <Container maxW='4xl' mt={3} style={{ minHeight: '100vh' }}>
-            <Box textAlign="left">
-                <Grid p={2} alignItems='left'>
-                    <h1 style={{ textAlign: 'center', fontWeight: '600', fontSize: '25px' }}>Connect</h1>
-                    <Box mt={5} textAlign="center">
-                        Continue with your provider
-                    </Box>
-                </Grid>
-                <Center h='100px' color='white'>
-                    {/* <Button minW="100%" colorScheme='orange' variant='solid'>
-                        Metamask
-                    </Button> */}
-                    <Badge colorScheme='orange'>In development</Badge>
-                </Center>
-            </Box>
+        <Container maxW='4xl' minH='100vh'>
+            <Flex
+                minH={'60vh'}
+                align={'center'}
+                justify={'center'}>
+                <Box textAlign="left" spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                    <Grid p={2} alignItems='left' mb={5}>
+                        <h1 style={{ textAlign: 'center', fontWeight: '600', fontSize: '25px' }}>Connect</h1>
+                        <Box mt={5} textAlign="center">
+                            Choose where you want to add our bot. <br />
+                            <Badge colorScheme={'orange'}>In development</Badge>
+                        </Box>
+                    </Grid>
+                    <Center h='150px'>
+                        <Stack spacing={2} align={'center'} maxW={'md'} w={'full'}>
+                            <Button w={'full'}  colorScheme={'messenger'} leftIcon={<SiMessenger />} disabled>
+                                <Center>
+                                    <Text>Add to Messenger</Text>
+                                </Center>
+                            </Button>
+                            <Button w={'full'}  colorScheme={'telegram'} leftIcon={<SiTelegram />} disabled>
+                                <Center>
+                                    <Text>Add to Telegram</Text>
+                                </Center>
+                            </Button>
+                            <Button w={'full'}  colorScheme={'orange'} leftIcon={<SiSlack />} disabled>
+                                <Center> 
+                                    <Text>Add to Slack</Text>
+                                </Center>
+                            </Button>
+                        </Stack>
+                        {/* <Badge colorScheme='orange'>In development</Badge> */}
+                    </Center>
+                </Box>
+            </Flex>
         </Container>
     )
 }
